f2aa44153b573754de716e75dd945a29b681752b
Duniesqui Rodriguez <dunierf@gmail.com>
2024-01-16 22:53:34 -0800

    Icons and pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Pages
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { UsersPageComponent } from './pages/users-page/users-page.component';
+import { UserPageComponent } from './pages/user-page/user-page.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+
+export const routes: Routes = [
+  {
+    path: 'users',
+    component: UsersPageComponent,
+    title: 'Users'
+  },
+  {
+    path: 'user',
+    component: UserPageComponent,
+    title: 'User'
+  },
+  {
+    path: 'products',
+    component: ProductsPageComponent,
+    title: 'Products'
+  },
+  {
+    path: 'product',
+    component: ProductPageComponent,
+    title: 'Product'
+  },
+  {
+    path: '**',
+    component: HomePageComponent,
+    title: 'Dashboard'
+  }
+
+];
