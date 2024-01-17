8a98ade1d80b2f10f2f52bee13d0de373483a47b
Duniesqui Rodriguez <dunierf@gmail.com>
2024-01-17 14:44:53 -0800

    Users page, not finished yet

--- src/app/pages/users-page/users-page.component.ts
+++ src/app/pages/users-page/users-page.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Models
+import { User } from './../../core/models/user/user.model';
+
+// Services
+import { UserService } from './../../core/services/user/user.service';
+
+// Components
+import { DataGridComponent } from '../../components/data-grid/data-grid.component';
 
 @Component({
   selector: 'app-users-page',
   standalone: true,
-  imports: [],
+  imports: [DataGridComponent],
   templateUrl: './users-page.component.html',
   styleUrl: './users-page.component.css'
 })
-export class UsersPageComponent {
+export class UsersPageComponent implements OnInit {
+
+  items: User[] = [];
+
+  constructor(private userService: UserService) {
+
+  }
+
+  ngOnInit(): void {
+    this.userService.getAll().subscribe({
+      next: (response: { data: User[] }) => {
+        this.items = response.data;
+      },
+      error: err => {
+
+      }
+    });
+  }
 
 }
