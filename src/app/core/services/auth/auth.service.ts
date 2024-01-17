e818a23db1e905c88b338f6a79c27a5e02961746
Duniesqui Rodriguez <dunierf@gmail.com>
2024-01-17 14:55:06 -0800

    Auth service

---
+++ src/app/core/services/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+// Environment
+import { environment } from '../../../../environments/environment';
+
+// Model
+import { User } from './../../models/user/user.model';
+
+
+@Injectable()
+export class AuthService {
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  login(email: string, password: string) : Observable<{ token: string }> {
+    const url = environment.apiUrl + 'auth/login';
+    return this.http.post<{ token: string }>(url, { email: email, password: password });
+  }
+
+  logout() : Observable<any> {
+    const url = environment.apiUrl + 'auth/logout';
+    return this.http.delete(url);
+  }
+}
