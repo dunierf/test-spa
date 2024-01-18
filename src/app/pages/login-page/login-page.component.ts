b057c6235b0f0e40a6cdbd5f4c9f0e2036c9d7b2
Duniesqui Rodriguez <dunierf@gmail.com>
2024-01-17 23:40:54 -0800

    Disabling SSR and pre-rendder, some problems with session storage

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 
+// Services
+import { AuthService } from './../../core/services/auth/auth.service';
+
+// Components
+import { LoginComponent } from '../../components/login/login.component';
+
+
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+
+  constructor(private authService: AuthService) {
+
+  }
+
+  onAuthenticationSucceeded(token: string) {
+    sessionStorage.setItem('token', token);
+  }
+
+  onAuthenticationFailed(msg: string) {
+    sessionStorage.removeItem('token');
+  }
 
 }
