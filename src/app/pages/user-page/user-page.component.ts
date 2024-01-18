f8aa9d39c497f04594cf0298e74a90ee0bb35f70
Duniesqui Rodriguez <dunierf@gmail.com>
2024-01-18 07:25:15 -0800

    Trying to fetch user by id

--- src/app/pages/user-page/user-page.component.ts
+++ src/app/pages/user-page/user-page.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 // Models
 import { User } from './../../core/models/user/user.model';
 
 // Services
 import { UserService } from './../../core/services/user/user.service';
+
 
 @Component({
   selector: 'app-user-page',
@@ -13,19 +15,39 @@ import { UserService } from './../../core/services/user/user.service';
   templateUrl: './user-page.component.html',
   styleUrl: './user-page.component.css'
 })
-export class UserPageComponent {
+export class UserPageComponent implements OnInit {
 
-  user: User = {
-    id: 0,
-    name: '',
-    email: '',
-    roles: []
-  };
+  user: User | undefined = undefined;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private route: ActivatedRoute) {
 
   }
 
+  newUser() {
+    this.user = {
+      id: 0,
+      name: '',
+      email: '',
+      roles: []
+    };
+  }
 
+  ngOnInit(): void {
+    this.route.params.subscribe( params => {
+      const id = params['id'] ? parseInt(params['id']) : 0;
 
+      if (id) {
+        this.userService.getById(id).subscribe({
+          next: (user: User) => {
+            this.user = user;
+          },
+          error: err => {
+            this.newUser();
+          },
+        })
+      }
+      else
+        this.newUser();
+    });
+  }
 }
